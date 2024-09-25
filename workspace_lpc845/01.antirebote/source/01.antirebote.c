a9021658e62c5553626a604027fec5ddebf40293
TobiasRodriguezVanini <81983303+TobiasRodriguezVanini@users.noreply.github.com>
2024-09-25 10:08:54 -0300

    feat: antirebote arreglado

--- workspace_lpc845/01.antirebote/source/01.antirebote.c
+++ workspace_lpc845/01.antirebote/source/01.antirebote.c
@@ -3,37 +3,35 @@
 #include "pin_mux.h"
 #include "fsl_debug_console.h"
 
-// Etiqueta para el LED azul
-#define LED_RED
-#define USER_BUTTON
+#define LED_RED	2
+#define USER_BOTTON 4
 
-
-/*
- * @brief   Application entry point.
-*/
 int main(void) {
-	// Inicializacion
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
 
-    // Estructura de configuracion para salida
-    gpio_pin_config_t config = { kGPIO_DigitalOutput, 2 };
-    gpio_pin_config_t config1 = { kGPIO_DigitalInput, 4 };
+    gpio_pin_config_t config = { kGPIO_DigitalOutput, 1 };
+    gpio_pin_config_t config1 = { kGPIO_DigitalInput, 0};
 
-    GPIO_PortInit(GPIO, 1);
     GPIO_PortInit(GPIO, 0);
+    GPIO_PortInit(GPIO, 1);
 
-    GPIO_PinInit(GPIO, 2, LED_RED, &config);
-    GPIO_PinInit(GPIO, 4, USER_BUTTOM, &config1);
+    GPIO_PinInit(GPIO, 1, LED_RED, &config);
+    GPIO_PinInit(GPIO, 0, USER_BOTTON, &config1);
 
-    while(1){
-    	if (GPIO_PinRead(GPIO,0 USER_BUTTOM)==1){
-    		for(uint32 t i=0 <20000; i++)
-    		(GPIO_PinWrite(GPIO,1,LED_RED);
+    while(1) {
+    	if (GPIO_PinRead(GPIO, 0, USER_BOTTON) == 1){
+    		for(uint32_t i = 0; i < 20000; i++);
+    		if (GPIO_PinRead(GPIO, 0, USER_BOTTON) == 1){
+    			GPIO_PinWrite(GPIO, 1, LED_RED, 1);
+    		}
+    		else{
+    			GPIO_PinWrite(GPIO, 1, LED_RED, 0);
+    		}
     	}
-    	else{
-    		GPIO_PinWrite(GPIO,1, LED_RED, 0);
-    	}
+    	for(uint32_t i = 0; i < 1000; i++);
+
     }
+    return 0;
 }
-return 0;
+
